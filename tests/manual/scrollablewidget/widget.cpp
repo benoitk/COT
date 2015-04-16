0d876abeb3af7be7250719d765c59a40316a544a
Montel Laurent <laurent.montel@kdab.com>
2015-04-16 14:28:50 +0200

    Improve test apps

--- tests/manual/scrollablewidget/widget.cpp
+++ tests/manual/scrollablewidget/widget.cpp
@@ -3,6 +3,7 @@
 #include <QVBoxLayout>
 #include <QToolBar>
 #include <QAction>
+#include <QLabel>
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
@@ -11,9 +12,19 @@ Widget::Widget(QWidget *parent)
     QToolBar *bar = new QToolBar(this);
     vbox->addWidget(bar);
 
+    QWidget *myWidget = new QWidget;
+    QVBoxLayout *myWidgetLayout = new QVBoxLayout;
+    myWidget->setLayout(myWidgetLayout);
+    for (int i = 0; i < 100 ; ++i) {
+        QLabel *label = new QLabel(QStringLiteral("label %1").arg(i));
+        myWidgetLayout->addWidget(label);
+    }
+
     CScrollableWidget *scrollablewidget = new CScrollableWidget(this);
+    scrollablewidget->setWidget(myWidget);
     vbox->addWidget(scrollablewidget);
     bar->addAction(scrollablewidget->moveUp());
     bar->addAction(scrollablewidget->moveDown());
+
 }
 
