869b6ab3fe83ac23679fe1dc1e321ad9fd1dbd96
Filipe Azevedo <filipe.azevedo@kdab.com>
2015-04-23 18:33:41 +0200

    Automate: Add fake api to allow implementing streams/measures UI.

--- libcot/automate/CAutomate.h
+++ libcot/automate/CAutomate.h
@@ -3,7 +3,20 @@
 
 #include "IComObserver.h"
 
+// KDAB: Remove me
+#include "CVariableFloat.h"
+#include "CVariableBool.h"
+#include "CVariableString.h"
+#include "CVariableInt.h"
+#include "CVariableInputBool.h"
+#include "CVariableInputFloat.h"
+#include "CVariableInputInt.h"
+#include "CVariableOutputBool.h"
+#include "CVariableOutputFloat.h"
+#include "CVariableOutputInt.h"
+
 #include <QObject>
+#include <QStringList>
 #include "qmap.h"
 #include "qmutex.h"
 
@@ -33,6 +46,65 @@ public:
     QMap<QString, QList<QString>> getMapStreamsMeasures() const;
     void setMapStreamsMeasures(QMap<QString, QList<QString>>);
     CDisplayConf* getDisplayConf()const;
+
+    // KDAB: Needed api
+    template <typename T, typename Val>
+    void createVariable(const QString &name, const Val &val) {
+        T * t = new T;
+        t->setName(name);
+        t->setLabel(name);
+        t->setValue(val);
+        addVariable(name, t);
+    }
+
+    QStringList getStreams() {
+        return QStringList() << "stream_1" << "stream_2";
+    }
+
+    QStringList getStreamVariables(const QString &stream) {
+        static QMap<QString, QStringList> variables;
+
+        if (variables.isEmpty()) {
+            variables["stream_1"] = QStringList() << "active_state" << "status" << "defaut_eau";
+            variables["stream_2"] = QStringList() << "active_state_2" << "var_stream" << "measure";
+
+            createVariable<CVariableBool, bool>("active_state", true);
+            createVariable<CVariableInputBool, bool>("status", true);
+            createVariable<CVariableInputBool, bool>("defaut_eau", true);
+
+            createVariable<CVariableBool, bool>("active_state_2", true);
+            createVariable<CVariableString, QString>("var_stream", "stream 42");
+            createVariable<CVariableInputFloat, double>("measure", 2.00);
+        }
+
+        return variables.value(stream);
+    }
+
+    QMap<QString, QStringList> getStreamMeasures(const QString &stream) {
+        QMap<QString, QMap<QString, QStringList>> measures;
+
+        if (measures.isEmpty()) {
+            QMap<QString, QStringList> &stream1 = measures["stream_1"];
+            stream1["measure_1"] = QList<QString>() << "coeff" << "measure_silice";
+            stream1["measure_2"] = QList<QString>() << "coeff_2" << "measure_silice_2";
+
+            createVariable<CVariableFloat, double>("coeff", 6.00);
+            createVariable<CVariableFloat, double>("measure_silice", 6.00);
+            createVariable<CVariableFloat, double>("coeff_2", 2.00);
+            createVariable<CVariableFloat, double>("measure_silice_2", 2.00);
+
+            QMap<QString, QStringList> &stream2 = measures["stream_2"];
+            stream2["measure_3"] = QList<QString>() << "coeff_3" << "measure_silice_3";
+            stream2["measure_4"] = QList<QString>() << "coeff_4" << "measure_silice_4";
+
+            createVariable<CVariableFloat, double>("coeff_3", 6.00);
+            createVariable<CVariableFloat, double>("measure_silice_3", 6.00);
+            createVariable<CVariableFloat, double>("coeff_4", 6.00);
+            createVariable<CVariableFloat, double>("measure_silice_4", 2.00);
+        }
+
+        return measures.value(stream);;
+    }
 	//FIN API
 
 
