239cec6157907f690ef95e672219e127e1089daa
Benoit Dumas <benoit.dumas@kdab.com>
2015-04-24 16:08:43 +0200

    add handler to electrical test tab "Pumps"

--- libcot/gui/tools/electrical-tests/CElectricalTestsPumpsTab.cpp
+++ libcot/gui/tools/electrical-tests/CElectricalTestsPumpsTab.cpp
@@ -1,6 +1,10 @@
 #include "CElectricalTestsPumpsTab.h"
 #include "ui_CElectricalTestsPumpsTab.h"
 
+#include "IVariableUIHandler.h"
+#include "CAutomate.h"
+#include "CDisplayConf.h"
+
 CElectricalTestsPumpsTab::CElectricalTestsPumpsTab(QWidget *parent)
     : IElectricalTestsTab(parent)
     , ui(new Ui::CElectricalTestsPumpsTab)
@@ -10,9 +14,24 @@ CElectricalTestsPumpsTab::CElectricalTestsPumpsTab(QWidget *parent)
     ui->vbbButtons->addAction(CToolButton::ScrollDown, ui->swCentral->moveDown());
     connect(ui->vbbButtons->addAction(CToolButton::Back), &QAction::triggered,
             this, &IElectricalTestsTab::backTriggered);
+
+
+    m_elecTestPumpHandler = new IVariableUIHandler(ui->swCentral, this);
+    updateElecTestPump();
+    connect(CAutomate::getInstance(), &CAutomate::signalDisplayconfChanged,
+            this, &CElectricalTestsPumpsTab::updateElecTestPump);
 }
 
 CElectricalTestsPumpsTab::~CElectricalTestsPumpsTab()
 {
     delete ui;
+}
+
+void CElectricalTestsPumpsTab::updateElecTestPump()
+{
+    CAutomate *automate = CAutomate::getInstance();
+    CDisplayConf *displayConf = automate->getDisplayConf();
+    QMap<QString, QList<IVariable*> > screenElecVariables = displayConf->getMapForScreenElectricalTests();
+    IVariablePtrList screenElecPump = screenElecVariables.value(CDisplayConf::strElecPump);
+    m_elecTestPumpHandler->layout(screenElecPump);
 }
--- libcot/gui/tools/electrical-tests/CElectricalTestsPumpsTab.h
+++ libcot/gui/tools/electrical-tests/CElectricalTestsPumpsTab.h
@@ -7,6 +7,8 @@ namespace Ui {
 class CElectricalTestsPumpsTab;
 }
 
+class IVariableUIHandler;
+
 class CElectricalTestsPumpsTab : public IElectricalTestsTab
 {
     Q_OBJECT
@@ -15,8 +17,12 @@ public:
     explicit CElectricalTestsPumpsTab(QWidget *parent = Q_NULLPTR);
     ~CElectricalTestsPumpsTab();
 
+protected Q_SLOTS:
+    void updateElecTestPump();
+
 private:
     Ui::CElectricalTestsPumpsTab *ui;
+    IVariableUIHandler *m_elecTestPumpHandler;
 };
 
 #endif // CELECTRICALTESTSPUMPSTAB_H
