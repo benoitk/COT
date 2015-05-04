391c3a1c8df5f280182f2ba89cec4335b4b26927
Filipe Azevedo <filipe.azevedo@kdab.com>
2015-05-04 16:34:31 +0200

    COT-10: Implement ConfiguratorUIHandler.

--- libcot/gui/ConfiguratorUIHandler.cpp
+++ libcot/gui/ConfiguratorUIHandler.cpp
@@ -1,21 +1,181 @@
 #include "ConfiguratorUIHandler.h"
 #include "CGenericItemSelector.h"
+#include "CKeyboardDialog.h"
+#include "CNumericalKeyboardDialog.h"
+#include "CAutomate.h"
+#include "CModelExtensionCard.h"
 #include "CVariableString.h"
+#include "IOrgan.h"
+#include "IAction.h"
 
 namespace {
 typedef QPair<QString, QVariant> PairStringString;
 typedef QPair<QString, PairStringString> PairPairStringString;
 typedef QList<PairPairStringString> PairPairStringStringList;
 
+IVariablePtr build(const QString& name) {
+    IVariable *ivar = new CVariableString;
+    ivar->setName(name);
+    ivar->setLabel(name);
+    return ivar;
+}
+
+IVariablePtr build(const QString& name, const QString &label) {
+    IVariable *ivar = new CVariableString;
+    ivar->setName(name);
+    ivar->setLabel(label);
+    return ivar;
+}
+
+IVariablePtr build(const QString& name, const QString &label, const QVariant &value) {
+    IVariable *ivar = new CVariableString;
+    ivar->setName(name);
+    ivar->setLabel(label);
+    ivar->setValue(value);
+    return ivar;
+}
+
+IVariablePtrList build(const QStringList& names) {
+    IVariablePtrList ivars;
+
+    foreach (const QString& name, names) {
+        ivars << build(name);
+    }
+
+    return ivars;
+}
+
 IVariablePtrList build(const PairPairStringStringList& pairs) {
     IVariablePtrList ivars;
 
     foreach (const PairPairStringString& pair, pairs) {
-        IVariable *ivar = new CVariableString;
-        ivar->setName(pair.first);
-        ivar->setLabel(pair.second.first);
-        ivar->setValue(pair.second.second);
-        ivars << ivar;
+        ivars << build(pair.first, pair.second.first, pair.second.second);
+    }
+
+    return ivars;
+}
+
+IVariablePtrList buildActionType() {
+    // KDAB_TODO: No customer api so let fake
+    return build({
+                     {"calc_coef", qMakePair(ConfiguratorUIHandler::tr("Calc Coef"), 1)},
+                     {"calc_rien", qMakePair(ConfiguratorUIHandler::tr("Calc Rien"), 2)},
+                     {"bloc", qMakePair(ConfiguratorUIHandler::tr("Bloc"), 1000)}
+                 });
+}
+
+IVariablePtrList buildVariableTypes() {
+    return build({
+                     {"type_bool", qMakePair(ConfiguratorUIHandler::tr("Boolean"), type_bool)},
+                     {"type_float", qMakePair(ConfiguratorUIHandler::tr("Float"), type_float)},
+                     {"type_int", qMakePair(ConfiguratorUIHandler::tr("Integer"), type_int)},
+                     {"type_string", qMakePair(ConfiguratorUIHandler::tr("String"), type_string)},
+                     {"type_stream", qMakePair(ConfiguratorUIHandler::tr("Stream"), type_stream)},
+                     {"type_unknow", qMakePair(ConfiguratorUIHandler::tr("Unknown"), type_unknow)}
+                 });
+}
+
+IVariablePtrList buildOrganTypes() {
+    return build({
+                     {"VariableOrganTypeNone", qMakePair(ConfiguratorUIHandler::tr("None"), VariableOrganTypeNone)},
+                     {"VariableOrganTypeInput", qMakePair(ConfiguratorUIHandler::tr("Input"), VariableOrganTypeInput)},
+                     {"VariableOrganTypeOutput", qMakePair(ConfiguratorUIHandler::tr("Output"), VariableOrganTypeOutput)}
+                 });
+}
+
+IVariablePtrList buildCycleTypes() {
+    return build({
+                     {"CYCLE_MESURE", qMakePair(ConfiguratorUIHandler::tr("Measure"), CYCLE_MESURE)},
+                     {"CYCLE_AUTONOME", qMakePair(ConfiguratorUIHandler::tr("Autonome"), CYCLE_AUTONOME)},
+                     {"CYCLE_MAINTENANCE", qMakePair(ConfiguratorUIHandler::tr("Maintenance"), CYCLE_MAINTENANCE)},
+                     {"CYCLE_PAUSE", qMakePair(ConfiguratorUIHandler::tr("Pause"), CYCLE_PAUSE)}
+                 });
+}
+
+IVariablePtrList buildStreams() {
+    // KDAB_TODO: No customer api so let fake
+    CAutomate *automate = CAutomate::getInstance();
+    return build(automate->getMapStreamsMeasures().keys());
+}
+
+IVariablePtrList buildMeasures() {
+    // KDAB_TODO: No customer api so let fake
+    CAutomate *automate = CAutomate::getInstance();
+    const QMap<QString, QList<QString>> streams = automate->getMapStreamsMeasures();
+    IVariablePtrList ivars;
+
+    foreach (const QString &stream, streams.keys()) {
+        foreach (const QString &measure, streams[stream]) {
+            ivars << build(measure);
+        }
+    }
+
+    return ivars;
+}
+
+IVariablePtrList buildCycles() {
+    CAutomate *automate = CAutomate::getInstance();
+    const QList<ICycle *> cycles = automate->getListCycles();
+    IVariablePtrList ivars;
+
+    foreach (ICycle *cycle, cycles) {
+        ivars << build(cycle->getName(), cycle->getLbl());
+    }
+
+    return ivars;
+}
+
+IVariablePtrList buildActions() {
+    CAutomate *automate = CAutomate::getInstance();
+    const QList<IAction *> actions = automate->getListActions();
+    IVariablePtrList ivars;
+
+    foreach (IAction *action, actions) {
+        ivars << build(action->getName(), action->getLabel());
+    }
+
+    return ivars;
+}
+
+IVariablePtrList buildStreamsMeasures() {
+    // KDAB_TODO: No customer api so let fake
+    CAutomate *automate = CAutomate::getInstance();
+    const QMap<QString, QList<QString>> streams = automate->getMapStreamsMeasures();
+    IVariablePtrList ivars;
+
+    foreach (const QString &stream, streams.keys()) {
+        ivars << build(stream);
+
+        foreach (const QString &measure, streams[stream]) {
+            ivars << build(measure, QString("%1 > %2").arg(stream).arg(measure));
+        }
+    }
+
+    return ivars;
+}
+
+IVariablePtrList buildExtensions() {
+    CAutomate *automate = CAutomate::getInstance();
+    const QList<CModelExtensionCard *> cards = automate->getListExtensions();
+    IVariablePtrList ivars;
+
+    foreach (CModelExtensionCard *card, cards) {
+        ivars << build(card->getName(), card->getLabel());
+    }
+
+    return ivars;
+}
+
+IVariablePtrList buildOrgans() {
+    // KDAB_TODO: No customer api so let fake
+    CAutomate *automate = CAutomate::getInstance();
+    QList<CModelExtensionCard *> cards = automate->getListExtensions();
+    IVariablePtrList ivars;
+
+    foreach (CModelExtensionCard *card, cards) {
+        foreach (IOrgan *organ, card->getListOrgans()) {
+            ivars << build(organ->getName());
+        }
     }
 
     return ivars;
@@ -34,107 +194,234 @@ ConfiguratorUIHandler::ConfiguratorUIHandler(QObject *parent)
 
 int ConfiguratorUIHandler::selectActionType(int defaultValue)
 {
-    // KDAB_TODO: No customer api so let fake
-    IVariablePtrList ivars = build({
-                                       {"calc_coef", qMakePair(tr("Calc Coef"), 1)},
-                                       {"calc_rien", qMakePair(tr("Calc Rien"), 2)},
-                                       {"bloc", qMakePair(tr("Bloc"), 1000)}
-                                   });
+    IVariablePtrList ivars = buildActionType();
     CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select an action type"));
     dlg.setSelectedValue(defaultValue);
-    int r = -1;
+    int result = -1;
 
     if (dlg.exec()) {
-        r = dlg.selectedItem()->toInt();
+        result = dlg.selectedItem()->toInt();
     }
 
     free(ivars);
-    return r;
+    return result;
 }
 
-variableType ConfiguratorUIHandler::selectVariableType(int defaultValue)
+variableType ConfiguratorUIHandler::selectVariableType(variableType defaultValue)
 {
-    IVariablePtrList ivars = build({
-                                      {"type_bool", qMakePair(tr("Boolean"), 0)},
-                                      {"type_float", qMakePair(tr("Float"), 1)},
-                                      {"type_int", qMakePair(tr("Integer"), 2)},
-                                      {"type_string", qMakePair(tr("String"), 3)},
-                                      {"type_stream", qMakePair(tr("Stream"), 4)},
-                                      {"type_unknow", qMakePair(tr("Unknown"), 5)}
-                                  });
+    IVariablePtrList ivars = buildVariableTypes();
     CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select a variable type"));
     dlg.setSelectedValue(defaultValue);
-    variableType r = type_unknow;
+    variableType result = defaultValue;
 
     if (dlg.exec()) {
-        r = variableType(dlg.selectedItem()->toInt());
+        result = variableType(dlg.selectedItem()->toInt());
     }
 
     free(ivars);
-    return r;
+    return result;
 }
 
-VariableOrganType ConfiguratorUIHandler::selectOrganType(int defaultValue)
+VariableOrganType ConfiguratorUIHandler::selectOrganType(VariableOrganType defaultValue)
 {
+    IVariablePtrList ivars = buildOrganTypes();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select an organ type type"));
+    dlg.setSelectedValue(defaultValue);
+    VariableOrganType result = defaultValue;
 
+    if (dlg.exec()) {
+        result = VariableOrganType(dlg.selectedItem()->toInt());
+    }
+
+    free(ivars);
+    return result;
 }
 
-int ConfiguratorUIHandler::selectCycleType(int defaultValue)
+eTypeCycle ConfiguratorUIHandler::selectCycleType(eTypeCycle defaultValue)
 {
+    IVariablePtrList ivars = buildCycleTypes();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select a cycle type"));
+    dlg.setSelectedValue(defaultValue);
+    eTypeCycle result = defaultValue;
 
+    if (dlg.exec()) {
+        result = eTypeCycle(dlg.selectedItem()->toInt());
+    }
+
+    free(ivars);
+    return result;
 }
 
-QString ConfiguratorUIHandler::selectVariable(const QString &defaultValue)
+QString ConfiguratorUIHandler::selectVariable(const QString &defaultName)
 {
+    CAutomate *automate = CAutomate::getInstance();
+    IVariablePtrList ivars = automate->getMapVariables().values();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select a variable"));
+    dlg.setSelectedValue(defaultName);
+    QString result = defaultName;
 
+    if (dlg.exec()) {
+        result = dlg.selectedItem()->getName();
+    }
+
+    return result;
 }
 
-QString ConfiguratorUIHandler::selectStream(const QString &defaultValue)
+QString ConfiguratorUIHandler::selectStream(const QString &defaultName)
 {
+    IVariablePtrList ivars = buildStreams();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select a stream"));
+    dlg.setSelectedValue(defaultName);
+    QString result = defaultName;
 
+    if (dlg.exec()) {
+        result = dlg.selectedItem()->getName();
+    }
+
+    free(ivars);
+    return result;
 }
 
-QString ConfiguratorUIHandler::selectMeasure(const QString &defaultValue)
+QString ConfiguratorUIHandler::selectMeasure(const QString &defaultName)
 {
+    IVariablePtrList ivars = buildMeasures();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select a measure"));
+    dlg.setSelectedValue(defaultName);
+    QString result = defaultName;
 
+    if (dlg.exec()) {
+        result = dlg.selectedItem()->getName();
+    }
+
+    free(ivars);
+    return result;
 }
 
-QString ConfiguratorUIHandler::selectCycle(const QString &defaultValue)
+QString ConfiguratorUIHandler::selectCycle(const QString &defaultName)
 {
+    IVariablePtrList ivars = buildCycles();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select a cycle"));
+    dlg.setSelectedValue(defaultName);
+    QString result = defaultName;
 
+    if (dlg.exec()) {
+        result = dlg.selectedItem()->getName();
+    }
+
+    free(ivars);
+    return result;
 }
 
-QString ConfiguratorUIHandler::selectAction(const QString &defaultValue)
+QString ConfiguratorUIHandler::selectAction(const QString &defaultName)
 {
+    IVariablePtrList ivars = buildActions();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select an action"));
+    dlg.setSelectedValue(defaultName);
+    QString result = defaultName;
 
+    if (dlg.exec()) {
+        result = dlg.selectedItem()->getName();
+    }
+
+    free(ivars);
+    return result;
 }
 
-QString ConfiguratorUIHandler::selectStreamOrMeasure(const QString &defaultValue)
+QString ConfiguratorUIHandler::selectStreamOrMeasure(const QString &defaultName)
 {
+    IVariablePtrList ivars = buildStreamsMeasures();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select a measure or measure"));
+    dlg.setSelectedValue(defaultName);
+    QString result = defaultName;
 
+    if (dlg.exec()) {
+        result = dlg.selectedItem()->getName();
+    }
+
+    free(ivars);
+    return result;
 }
 
-QString ConfiguratorUIHandler::selectExtension(const QString &defaultValue)
+QString ConfiguratorUIHandler::selectExtension(const QString &defaultName)
 {
+    IVariablePtrList ivars = buildExtensions();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select an extension"));
+    dlg.setSelectedValue(defaultName);
+    QString result = defaultName;
 
+    if (dlg.exec()) {
+        result = dlg.selectedItem()->getName();
+    }
+
+    free(ivars);
+    return result;
 }
 
-QString ConfiguratorUIHandler::selectOrgan(const QString &defaultValue)
+QString ConfiguratorUIHandler::selectOrgan(const QString &defaultName)
 {
+    IVariablePtrList ivars = buildOrgans();
+    CGenericItemSelector dlg(ivars);
+    dlg.setTitle(tr("Select an organ"));
+    dlg.setSelectedValue(defaultName);
+    QString result = defaultName;
 
+    if (dlg.exec()) {
+        result = dlg.selectedItem()->getName();
+    }
+
+    free(ivars);
+    return result;
 }
 
 QString ConfiguratorUIHandler::enterText(const QString &defaultValue)
 {
+    CKeyboardDialog dlg;
+    dlg.setTitle(tr("Enter a new value"));
+    dlg.setStringValue(defaultValue);
+    QString result = defaultValue;
 
+    if (dlg.exec()) {
+        result = dlg.stringValue();
+    }
+
+    return result;
 }
 
 int ConfiguratorUIHandler::enterInteger(int defaultValue)
 {
+    CNumericalKeyboardDialog dlg(CNumericalKeyboardWidget::Integer);
+    dlg.setTitle(tr("Enter a new value"));
+    dlg.setIntegerValue(defaultValue);
+    int result = defaultValue;
 
+    if (dlg.exec()) {
+        result = dlg.integerValue();
+    }
+
+    return result;
 }
 
 double ConfiguratorUIHandler::enterDouble(double defaultValue)
 {
+    CNumericalKeyboardDialog dlg(CNumericalKeyboardWidget::Double);
+    dlg.setTitle(tr("Enter a new value"));
+    dlg.setDoubleValue(defaultValue);
+    double result = defaultValue;
 
+    if (dlg.exec()) {
+        result = dlg.doubleValue();
+    }
+
+    return result;
 }
--- libcot/gui/ConfiguratorUIHandler.h
+++ libcot/gui/ConfiguratorUIHandler.h
@@ -4,6 +4,7 @@
 #include <QObject>
 
 #include "IVariable.h"
+#include "ICycle.h"
 
 class ConfiguratorUIHandler : public QObject
 {
@@ -16,34 +17,34 @@ public:
     // return an action type: calc_coef, cal_rien, bloc
     int selectActionType(int defaultValue = -1);
     // Select a variable type (float, int, string)
-    variableType selectVariableType(int defaultValue = -1);
+    variableType selectVariableType(variableType defaultValue = type_unknow);
     // Select a variable organ type (input, output, none)
-    VariableOrganType selectOrganType(int defaultValue = -1);
+    VariableOrganType selectOrganType(VariableOrganType defaultValue = VariableOrganTypeNone);
     // Select a cycle type (maintenance, autonome, pause...)
-    int selectCycleType(int defaultValue = -1);
+    eTypeCycle selectCycleType(eTypeCycle defaultValue = CYCLE_INVALID);
     // Select a variable
-    QString selectVariable(const QString &defaultValue = QString());
+    QString selectVariable(const QString &defaultName = QString());
     // Select a stream name
-    QString selectStream(const QString &defaultValue = QString());
+    QString selectStream(const QString &defaultName = QString());
     // Select a measure
-    QString selectMeasure(const QString &defaultValue = QString());
+    QString selectMeasure(const QString &defaultName = QString());
     // Select a cycle
-    QString selectCycle(const QString &defaultValue = QString());
+    QString selectCycle(const QString &defaultName = QString());
     // Select an action
-    QString selectAction(const QString &defaultValue = QString());
+    QString selectAction(const QString &defaultName = QString());
     // Select a stream and/or measure
-    QString selectStreamOrMeasure(const QString &defaultValue = QString());
+    QString selectStreamOrMeasure(const QString &defaultName = QString());
     // Select extension
-    QString selectExtension(const QString &defaultValue = QString());
+    QString selectExtension(const QString &defaultName = QString());
     // Select organ
-    QString selectOrgan(const QString &defaultValue = QString());
+    QString selectOrgan(const QString &defaultName = QString());
 
     // Ask user to enter a text value
     QString enterText(const QString &defaultValue = QString());
     // Ask user to enter an int value
-    int enterInteger(int defaultValue = -1);
+    int enterInteger(int defaultValue = 0);
     // Ask user to enter a double value
-    double enterDouble(double defaultValue = -1.0);
+    double enterDouble(double defaultValue = 0);
 };
 
 #endif // CONFIGURATORUIHANDLER_H
