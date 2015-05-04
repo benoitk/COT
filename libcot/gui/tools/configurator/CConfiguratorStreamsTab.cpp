bc25ba31f4f5befda35957b0aa400f7f0db9da69
Montel Laurent <laurent.montel@kdab.com>
2015-05-04 10:34:47 +0200

    Allow to add variable here too (COT-13)

--- libcot/gui/tools/configurator/CConfiguratorStreamsTab.cpp
+++ libcot/gui/tools/configurator/CConfiguratorStreamsTab.cpp
@@ -1,15 +1,20 @@
 #include "CConfiguratorStreamsTab.h"
 #include "ui_CConfiguratorStreamsTab.h"
 
+#include <IVariableUIHandler.h>
+
 CConfiguratorStreamsTab::CConfiguratorStreamsTab(QWidget *parent)
     : IConfiguratorTab(parent)
     , ui(new Ui::CConfiguratorStreamsTab)
 {
     ui->setupUi(this);
+    connect(ui->vbbButtons->addAction(CToolButton::AddVariable), &QAction::triggered,
+            this, &CConfiguratorStreamsTab::addVariable);
     ui->vbbButtons->addAction(CToolButton::ScrollUp, ui->swCentral->moveUp());
     ui->vbbButtons->addAction(CToolButton::ScrollDown, ui->swCentral->moveDown());
     connect(ui->vbbButtons->addAction(CToolButton::Back), &QAction::triggered,
             this, &IConfiguratorTab::backTriggered);
+    m_ivariableUIHandler = new IVariableUIHandler(ui->swCentral, this);
 }
 
 CConfiguratorStreamsTab::~CConfiguratorStreamsTab()
@@ -17,3 +22,7 @@ CConfiguratorStreamsTab::~CConfiguratorStreamsTab()
     delete ui;
 }
 
+void CConfiguratorStreamsTab::addVariable()
+{
+
+}
--- libcot/gui/tools/configurator/CConfiguratorStreamsTab.h
+++ libcot/gui/tools/configurator/CConfiguratorStreamsTab.h
@@ -5,13 +5,17 @@
 namespace Ui {
 class CConfiguratorStreamsTab;
 }
+class IVariableUIHandler;
 class CConfiguratorStreamsTab : public IConfiguratorTab
 {
     Q_OBJECT
 public:
     explicit CConfiguratorStreamsTab(QWidget *parent = Q_NULLPTR);
     ~CConfiguratorStreamsTab();
+private slots:
+    void addVariable();
 private:
+    IVariableUIHandler *m_ivariableUIHandler;
     Ui::CConfiguratorStreamsTab *ui;
 };
 
