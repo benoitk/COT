ab69ee50fe9dff06e6cb5019085b82f16f3ca125
Montel Laurent <laurent.montel@kdab.com>
2015-05-11 16:30:41 +0200

    Remove virtual keyword

--- libcot/gui/tools/configurator/cycles/CConfiguratorCycleTabUIHandler.h
+++ libcot/gui/tools/configurator/cycles/CConfiguratorCycleTabUIHandler.h
@@ -18,12 +18,12 @@ public:
 protected:
     QHash<QString, IVariable *> m_internalVariables;
 
-    virtual IVariable *getVariable(const QString &name) Q_DECL_OVERRIDE;
-    virtual int columnCount() const Q_DECL_OVERRIDE;
-    virtual QWidget *createWidget(int column, IVariable *ivar) Q_DECL_OVERRIDE;
-    virtual void rowInserted(const Row &row, IVariable *ivar) Q_DECL_OVERRIDE;
-    virtual void rowChanged(const Row &row, IVariable *ivar) Q_DECL_OVERRIDE;
-    virtual void rowAboutToBeDeleted(const Row &row, IVariable *ivar) Q_DECL_OVERRIDE;
+    IVariable *getVariable(const QString &name) Q_DECL_OVERRIDE;
+    int columnCount() const Q_DECL_OVERRIDE;
+    QWidget *createWidget(int column, IVariable *ivar) Q_DECL_OVERRIDE;
+    void rowInserted(const Row &row, IVariable *ivar) Q_DECL_OVERRIDE;
+    void rowChanged(const Row &row, IVariable *ivar) Q_DECL_OVERRIDE;
+    void rowAboutToBeDeleted(const Row &row, IVariable *ivar) Q_DECL_OVERRIDE;
 
 protected slots:
     void slotEditClicked();
