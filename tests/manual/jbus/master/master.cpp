2412b443617e89589cfe75b7eb03d0af814db2d3
Milian Wolff <milian.wolff@kdab.com>
2015-05-27 18:51:41 +0200

    Fix compiler warnings

--- tests/manual/jbus/master/master.cpp
+++ tests/manual/jbus/master/master.cpp
@@ -31,7 +31,7 @@ int main()
         printf("received query of length %d, sending reply.\n", rc);
 
         rc = modbus_reply(ctx, query, rc, &mapping);
-        for (int i = 0; i < sizeof(bits); ++i) {
+        for (uint i = 0; i < sizeof(bits); ++i) {
             printf("i = %d, b = %x, bi = %x\n", i, bits[i], bits_input[i]);
         }
         if (rc == -1) {
