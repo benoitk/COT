f547bf191a87f4f098c7f0a7a941f08608dec1f5
Filipe Azevedo <filipe.azevedo@kdab.com>
2015-05-27 17:43:12 +0200

    Use ok button instead of back one.

--- libcot/gui/widgets/CMessageBox.cpp
+++ libcot/gui/widgets/CMessageBox.cpp
@@ -1,8 +1,9 @@
 #include "CMessageBox.h"
-
 #include "CToolButton.h"
+#include "CVerticalButtonBar.h"
 
 #include <QLabel>
+#include <QAction>
 
 CMessageBox::CMessageBox(const QString &message, QWidget *parent)
     : IWindow(parent),
@@ -22,4 +23,8 @@ void CMessageBox::setupWindow()
     label->setText(m_message);
 
     setMainWidget(label);
+
+    QAction *okAction = buttonBar()->addAction(CToolButton::Ok);
+    buttonBar()->removeAction(CToolButton::Back);
+    connect(okAction, &QAction::triggered, this, &QWidget::close);
 }
