21894d90d08934f440589708260e6bb67358a0f4
Montel Laurent <laurent.montel@kdab.com>
2015-06-02 16:10:19 +0200

    Add doc in this class

--- libcot/graphics/CGraphicsWidget.h
+++ libcot/graphics/CGraphicsWidget.h
@@ -14,13 +14,40 @@ public:
     explicit CGraphicsWidget(QWidget *parent = Q_NULLPTR);
     ~CGraphicsWidget();
 
+    /*
+     * Clear the graphic and remove hash list
+     */
     void clear();
+
+    /*
+     * Change graphics limit.
+     */
     void changeLimits(double x1, double x2, double y1, double y2);
 
+    /*
+     * Add a new curve to graphics and return curve object
+     */
     KPlotObject *addCurve(const QList<QPointF> &listPoints, const QString &mesureName = QString(), const QColor &col = QColor());
-    static QColor createNewColor();
+
+    /*
+     * Create or update a curve based on measurename
+     * If measurename doesn't exist this function will create a new curve
+     * otherwise it will update curve data
+     */
     void addOrUpdateCurve(const QList<QPointF> &listPoints, const QString &mesureName);
+
+    /*
+     * Create or update a curve based on measurename
+     * If measurename doesn't exist this function will create a new curve
+     * otherwise it will update curve data
+     * It will add a new Y value (X is based on QDateTime).
+     */
     void addOrUpdateCurve(float value, const QString &mesureName);
+
+    /*
+     * Generate new randow color
+     */
+    static QColor createNewColor();
 private:
     KPlotObject *addCurve(double value, const QString &measureName, const QColor &col);
     void addPoint(float value, const QString &measureName, KPlotObject *curve);
