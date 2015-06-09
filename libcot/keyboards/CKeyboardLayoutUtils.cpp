94ed4bf56853c7242625489da6a068a527b6f2e4
David Faure <david.faure@kdab.com>
2015-06-09 13:26:29 +0200

    COT-4: Fix uppercase-3 letter in Russian keyboard

--- libcot/keyboards/CKeyboardLayoutUtils.cpp
+++ libcot/keyboards/CKeyboardLayoutUtils.cpp
@@ -118,7 +118,7 @@ QString CKeyboardLayoutUtils::frenchShiftMapping() const
 
 QString CKeyboardLayoutUtils::russianShiftMapping() const
 {
-    return QStringLiteral("1!2\"3И4;5%6:7?8*9(0)_-=+\\/");
+    return QStringLiteral("1!2\"3№4;5%6:7?8*9(0)_-=+\\/");
 }
 
 
