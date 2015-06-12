29e4c164ec460e703d2bc81be54db40b2b495651
David Faure <david.faure@kdab.com>
2015-06-12 17:52:58 +0200

    Add tabwidget to CMessageBox. Not sure about the title to use though.

--- libcot/gui/widgets/CMessageBox.cpp
+++ libcot/gui/widgets/CMessageBox.cpp
@@ -4,21 +4,28 @@
 #include <QLabel>
 #include <QAction>
 #include <QHBoxLayout>
+#include <QTabWidget>
 
 CMessageBox::CMessageBox(const QString &message, QWidget *parent)
     : QDialog(parent),
       m_message(message)
 {
-    QLabel *label = new QLabel(this);
+    QTabWidget *tabWidget = new QTabWidget(this);
+    QWidget *page = new QWidget(tabWidget);
+    tabWidget->addTab(page, tr("INFORMATION"));
+    QHBoxLayout *mainLayout = new QHBoxLayout(this);
+    mainLayout->addWidget(tabWidget);
+
+    QLabel *label = new QLabel(page);
     label->setText(m_message);
 
-    m_buttonBar = new CVerticalButtonBar(this);
+    m_buttonBar = new CVerticalButtonBar(page);
     QAction *okAction = m_buttonBar->addAction(CToolButton::Ok);
     connect(okAction, &QAction::triggered, this, &QWidget::close);
 
-    QHBoxLayout *hLayout = new QHBoxLayout(this);
-    hLayout->addWidget(label);
-    hLayout->addWidget(m_buttonBar);
+    QHBoxLayout *pageLayout = new QHBoxLayout(page);
+    pageLayout->addWidget(label);
+    pageLayout->addWidget(m_buttonBar);
 
     setProperty("modalStyle", true); // see cot.qss
 }
